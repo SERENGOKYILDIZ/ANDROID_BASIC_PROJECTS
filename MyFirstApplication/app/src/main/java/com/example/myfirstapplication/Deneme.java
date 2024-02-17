857f472f848d29eab38023b62f512cec07ce23fe
Semi Eren Gökyıldız <erengokyildiz@outlook.com.tr>
2024-02-17 18:50:32 +0300

    first commit

---
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/Deneme.java
@@ -0,0 +1,10 @@
+package com.example.myfirstapplication;
+
+public class Deneme
+{
+    public static  void main(String args[])
+    {
+        System.out.println("Hello World");
+        System.out.println("Semi Eren Gökyıldız");
+    }
+}
